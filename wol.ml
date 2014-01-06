9e9aa34a0a54f23ed42a0d85470ac4bb66b8290a
Piotr S. Staszewski <p.staszewski@gmail.com>
2014-01-06 23:20:41 +0100

    OCaml version cleanup

--- wol.ml
+++ wol.ml
@@ -23,8 +23,7 @@ let mactab =
     ("rpi.l",   "B8:27:EB:0D:EB:01")];;
 
 let parse_mac = fun mac ->
-  let mac_lst = List.map (fun x -> int_of_string ("0x" ^ x)) (Str.split (Str.regexp ":") mac) in
-    if List.length mac_lst <> 6 then raise Exit else mac_lst;;
+  List.map (fun x -> int_of_string ("0x" ^ x)) (Str.split (Str.regexp ":") mac);;
 
 let dup_list = fun lst times ->
   let lr = ref [] in
@@ -37,26 +36,24 @@ let make_payload = fun addr ->
     List.iteri (fun i x -> String.set buf i (char_of_int x)) payload;
     buf;;
 
-let send_payload = fun payload ->
-  let sock = socket PF_INET SOCK_DGRAM 0 and
-  target = ADDR_INET(inet_addr_of_string brdip, 9) in
-    setsockopt sock SO_BROADCAST true;
-    sendto sock payload 0 (String.length payload) [MSG_PEEK] target;;
-
 let () =
-  for i = 1 to Array.length Sys.argv - 1 do
-    let token = Sys.argv.(i) in
-      try
-        let addr = parse_mac @@
+  let sock = socket PF_INET SOCK_DGRAM 0
+  and target = ADDR_INET(inet_addr_of_string brdip, 9) in
+    setsockopt sock SO_BROADCAST true;
+    for i = 1 to Array.length Sys.argv - 1 do
+      let token = Sys.argv.(i) in
+      let addr = try
+        parse_mac @@
           try
             StringMap.find token mactab
           with Not_found ->
             token
-        in
+      with _ -> [] in
+        if List.length addr <> 6 then
+          Printf.printf "%s: parse error!\n" token
+        else
           let payload = make_payload addr in
             for x = 1 to 3 do
-              Printf.printf "%s -> %d\n" token (send_payload payload)
+              ignore @@ sendto sock payload 0 (String.length payload) [MSG_PEEK] target
             done
-      with _ ->
-        Printf.printf "%s: parse error!\n" token
-  done;;
+    done;;
