abb29956bfdacd23727ae7ca10428972d3a92007
Piotr S. Staszewski <p.staszewski@gmail.com>
2014-01-07 23:26:27 +0100

    Updated Ruby version

--- wol.rb
+++ wol.rb
@@ -10,33 +10,28 @@
 
 require 'socket'
 
-class WOL
+module WOL
   BRDCASTIP = '192.168.0.255'
   MACTAB = {
     'bebop.l' => '70:5A:B6:94:8F:59',
     'gamer.l' => '00:25:22:f4:0b:0e',
-    'lore.l' => '00:40:ca:6d:16:07',
-    'nox.l' => '00:40:63:D5:8B:65',
-    'rpi.l' => 'B8:27:EB:0D:EB:01'
+    'lore.l'  => '00:40:ca:6d:16:07',
+    'nox.l'   => '00:40:63:D5:8B:65',
+    'rpi.l'   => 'B8:27:EB:0D:EB:01',
   }
 
   @s = UDPSocket.new
   @s.setsockopt(Socket::SOL_SOCKET, Socket::SO_BROADCAST, true)
 
   def self.send(mac)
-    if mac.match(/..:..:..:..:..:../).nil?
-      nmac = MACTAB[mac]
-      unless nmac
-        STDERR.puts "#{mac} - failed to parse, packet not send."
-        return false
-      end
-      mac = nmac
-    end
+    mac = MACTAB[mac] if MACTAB.has_key? mac
+    return false unless mac.match(/^(\h\h:){5}\h\h$/i)
+
     payload = 0xff.chr*6 + mac.split(':').pack('H*'*6)*16
     3.times { @s.send(payload, 0, BRDCASTIP, 'discard') }
   end
 end
 
 if __FILE__ == $0
-  ARGV.each {|a| WOL.send(a) }
+  ARGV.each {|a| WOL.send(a) or STDERR.puts "#{a}: parse error!" }
 end
